e80dc5a6bfadc3ae8503bc44136f7bf962f4ca02
rhythmcache <153998419+rhythmcache@users.noreply.github.com>
2026-08-13 16:48:26 +0530

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -60,7 +60,7 @@ android {
     testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     buildConfigField("String", "USER_AGENT", "\"PayloadDumper-GUI/$versionName\"")
     buildConfigField("String", "GITHUB_URL", "\"https://github.com/rhythmcache\"")
-    buildConfigField("String", "TELEGRAM_URL", "\"https://t.me/dusk133\"")
+    buildConfigField("String", "TELEGRAM_URL", "\"https://t.me/tr1ple_fault\"")
     buildConfigField("String", "REPO_URL", "\"https://github.com/rhythmcache/payload-dumper-gui\"")
     buildConfigField(
         "String",
